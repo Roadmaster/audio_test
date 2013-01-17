93208bc1999f9071a4e4e9cecdfb098af13590f9
Daniel Manrique <Daniel Manrique roadmr@ubuntu.com>
2013-01-17 15:18:32 -0500

    Add a comment to p3test.py

--- p3test.py
+++ p3test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from gi.repository import Gst
 Gst.init(None)
-
+#This is just to test gst parsing and launching on python3.
 pipe = Gst.parse_launch("autoaudiosrc ! "
                         "audioconvert ! "
                         "audio/x-raw-int,channels=1,rate=(int)44100 ! audioresample ! wavenc ! filesink location=/tmp/a.wav")
